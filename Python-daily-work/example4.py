82d556f11bada1ce3f27e474d281ad8f8b2e5de1
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-03 08:48:18 +0530

    importing some libraries

---
+++ Python-daily-work/example4.py
@@ -0,0 +1,125 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello")
+hello
+a=input("Enter the ist value")
+Enter the ist value
+b=input("enter 2nd value")
+enter 2nd value
+c=a+b
+print(f'sum of{a} and{b} is {c}')
+sum of and is 
+12
+12
+23
+23
+23
+23
+import calendar
+import math
+import cmath
+cmath=srqt(-9)
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    cmath=srqt(-9)
+NameError: name 'srqt' is not defined
+cmath.sqrt(-9)
+3j
+cmath.sqrt(25)
+(5+0j)
+import math
+math.sqrt(25)
+5.0
+a=10
+b=20
+c=a+b
+print(c)
+30
+print("hello")
+hello
+print(a)
+10
+print(b)
+20
+print(c)
+30
+a=20
+b=50
+print("sum of two number is"c)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print("sum of two number is",c)
+sum of two number is 30
+a=input("enter ist number")
+enter ist number
+b=input("enter 2nd number")
+enter 2nd number
+c=a+b
+print("sum of two number is =",c)
+sum of two number is = 
+12
+12
+a=12
+b=14
+c=a+b
+print(c)
+26
+dir(calendar)
+['APRIL', 'AUGUST', 'Calendar', 'DECEMBER', 'Day', 'EPOCH', 'FEBRUARY', 'FRIDAY', 'HTMLCalendar', 'IllegalMonthError', 'IllegalWeekdayError', 'IntEnum', 'JANUARY', 'JULY', 'JUNE', 'LocaleHTMLCalendar', 'LocaleTextCalendar', 'MARCH', 'MAY', 'MONDAY', 'Month', 'NOVEMBER', 'OCTOBER', 'SATURDAY', 'SEPTEMBER', 'SUNDAY', 'THURSDAY', 'TUESDAY', 'TextCalendar', 'WEDNESDAY', '_EPOCH_ORD', '__all__', '__builtins__', '__cached__', '__doc__', '__file__', '__getattr__', '__loader__', '__name__', '__package__', '__spec__', '_colwidth', '_get_default_locale', '_locale', '_localized_day', '_localized_month', '_monthlen', '_nextmonth', '_prevmonth', '_spacing', 'c', 'calendar', 'datetime', 'day_abbr', 'day_name', 'different_locale', 'error', 'firstweekday', 'format', 'formatstring', 'global_enum', 'isleap', 'leapdays', 'main', 'mdays', 'month', 'month_abbr', 'month_name', 'monthcalendar', 'monthrange', 'prcal', 'prmonth', 'prweek', 'repeat', 'setfirstweekday', 'sys', 'timegm', 'warnings', 'week', 'weekday', 'weekheader']
+dir(math)
+['__doc__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'cbrt', 'ceil', 'comb', 'copysign', 'cos', 'cosh', 'degrees', 'dist', 'e', 'erf', 'erfc', 'exp', 'exp2', 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'isqrt', 'lcm', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'nextafter', 'perm', 'pi', 'pow', 'prod', 'radians', 'remainder', 'sin', 'sinh', 'sqrt', 'sumprod', 'tan', 'tanh', 'tau', 'trunc', 'ulp']
+dir(cmath)
+['__doc__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atanh', 'cos', 'cosh', 'e', 'exp', 'inf', 'infj', 'isclose', 'isfinite', 'isinf', 'isnan', 'log', 'log10', 'nan', 'nanj', 'phase', 'pi', 'polar', 'rect', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'tau']
+dir(builtins)
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    dir(builtins)
+NameError: name 'builtins' is not defined. Did you forget to import 'builtins'?
+dir(_builtins_)
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    dir(_builtins_)
+NameError: name '_builtins_' is not defined. Did you mean: '__builtins__'?
+yes
+Traceback (most recent call last):
+  File "<pyshell#42>", line 1, in <module>
+    yes
+NameError: name 'yes' is not defined
+import(builtins)
+SyntaxError: invalid syntax
+import builtins
+dir(builtins)
+['ArithmeticError', 'AssertionError', 'AttributeError', 'BaseException', 'BaseExceptionGroup', 'BlockingIOError', 'BrokenPipeError', 'BufferError', 'BytesWarning', 'ChildProcessError', 'ConnectionAbortedError', 'ConnectionError', 'ConnectionRefusedError', 'ConnectionResetError', 'DeprecationWarning', 'EOFError', 'Ellipsis', 'EncodingWarning', 'EnvironmentError', 'Exception', 'ExceptionGroup', 'False', 'FileExistsError', 'FileNotFoundError', 'FloatingPointError', 'FutureWarning', 'GeneratorExit', 'IOError', 'ImportError', 'ImportWarning', 'IndentationError', 'IndexError', 'InterruptedError', 'IsADirectoryError', 'KeyError', 'KeyboardInterrupt', 'LookupError', 'MemoryError', 'ModuleNotFoundError', 'NameError', 'None', 'NotADirectoryError', 'NotImplemented', 'NotImplementedError', 'OSError', 'OverflowError', 'PendingDeprecationWarning', 'PermissionError', 'ProcessLookupError', 'RecursionError', 'ReferenceError', 'ResourceWarning', 'RuntimeError', 'RuntimeWarning', 'StopAsyncIteration', 'StopIteration', 'SyntaxError', 'SyntaxWarning', 'SystemError', 'SystemExit', 'TabError', 'TimeoutError', 'True', 'TypeError', 'UnboundLocalError', 'UnicodeDecodeError', 'UnicodeEncodeError', 'UnicodeError', 'UnicodeTranslateError', 'UnicodeWarning', 'UserWarning', 'ValueError', 'Warning', 'WindowsError', 'ZeroDivisionError', '_', '__build_class__', '__debug__', '__doc__', '__import__', '__loader__', '__name__', '__package__', '__spec__', 'abs', 'aiter', 'all', 'anext', 'any', 'ascii', 'bin', 'bool', 'breakpoint', 'bytearray', 'bytes', 'callable', 'chr', 'classmethod', 'compile', 'complex', 'copyright', 'credits', 'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval', 'exec', 'exit', 'filter', 'float', 'format', 'frozenset', 'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input', 'int', 'isinstance', 'issubclass', 'iter', 'len', 'license', 'list', 'locals', 'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open', 'ord', 'pow', 'print', 'property', 'quit', 'range', 'repr', 'reversed', 'round', 'set', 'setattr', 'slice', 'sorted', 'staticmethod', 'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip']
+>>> num1=input("enter ist number")
+enter ist number12
+>>> num2=input("enter 2nd number")
+enter 2nd number90
+>>> print(num1+num2)
+1290
+>>> print(f'sum of two number'num1,num2)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(f'sum of two number is',num1,num2)
+sum of two number is 12 90
+>>> num3=num1+num2
+>>> print(num3)
+1290
+>>> a=(input("enter ist number"))
+enter ist number12
+>>> b=(input("enter 2nd number"))
+enter 2nd number80
+>>> c=(a+b)
+>>> print(c)
+1280
+>>> c=a+b
+>>> print(c)
+1280
+>>> num1=10
+>>> num2=20
+>>> sum=sum([num1,num2])
+>>> print("sum of ",num1 "and",num2,"is",sum)
+SyntaxError: invalid syntax
+>>> print(f'sum of two number is,num1,num2')
+sum of two number is,num1,num2
+>>> print(f'sum of two number is{num1}and {num2}is{num3}')
+sum of two number is10and 20is1290
+>>> print(f'sum of two number is{num1}and {num2}is')
+sum of two number is10and 20is
