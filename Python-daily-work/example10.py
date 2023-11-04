b40d90f681aeedf75f88c652b036b5373d064e47
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-05 05:28:40 +0530

    Add files via upload

---
+++ Python-daily-work/example10.py
@@ -0,0 +1,17 @@
+Name="newtamkashyap"
+rollno=101
+Class=12
+fee=2000.0
+#we can seperat two variable while using sep(seperation)statement.
+print(Name,sep="&")
+print(rollno,sep="&")
+print(Class,sep="&")
+print(fee,sep="&")
+
+
+#OR we can print in one line by using sepretion(sep)
+
+print(Name,rollno,Class,fee,sep="&")
+
+
+
