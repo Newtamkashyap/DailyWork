01e22418f01ec429ba5d6f9857f116eb75448dd1
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-05 04:46:24 +0530

    Add files via upload

---
+++ Python-daily-work/example9.py
@@ -0,0 +1,45 @@
+s1="python"
+print(s1)
+
+s2='pythons'
+print(s2)
+
+s3="python 3.24"
+print(s3)
+
+s4="45"
+print(s4)
+
+
+s5="45"
+n1=45
+n1+40
+print(s5)
+
+s6=(n1+40)
+print(s6)
+
+s7=int(s5)+n1
+print(s7)
+
+s8='python is a programming language'
+print(s8)
+
+s9='python is a "programing" language'
+print(s9)
+
+s10="python is a 'pure' object oriented language"
+print(s10)
+
+s11="""hello
+world
+how
+are
+you"""
+print(s11)
+
+print(10,20,30,40,50)
+print(s1,s2,s3,s4,s5,s6)
+
+
+
