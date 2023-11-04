666cb3aaad5ede9dbb5b4be337c36c6cf02444f7
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-05 03:42:57 +0530

    Add files via upload

---
+++ Python-daily-work/example8.py
@@ -0,0 +1,13 @@
+num1=input("enter ist value")
+num2=input("enter 2nd value")
+num3=int(num1)+int(num2)
+print(f'sum of{num1}and {num2} is : {num3}')
+a=1
+b=1
+c=a+b
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+
