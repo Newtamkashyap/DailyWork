07f410db30ca302167fbe2422392536a7cfe756a
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-06 03:48:47 +0530

    Add files via upload

---
+++ Python-daily-work/example18.py
@@ -0,0 +1,36 @@
+#changing diffrent different data types into boolean(bool) function.
+
+
+b1=bool(20)
+print(b1)
+
+b2=bool(-20)
+print(b2)
+
+b3=bool(True)
+print(b3)
+
+b4=bool(False)
+print(b4)
+
+
+b5=bool(1)
+print(b5)
+
+b6=bool(0)
+print(b6)
+
+b7=bool("hello")
+print(b7)
+
+b8=bool(1+2j)
+print(b8)
+
+b9=bool( 1.00)
+print(b9)
+
+b10=bool("false")
+print(b10)
+
+b11=bool("true")
+print(b11)
