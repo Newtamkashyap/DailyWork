f8a7fd59bd36c97889a725a4f9bd753aa4175a2e
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-06 00:43:28 +0530

    Add files via upload

---
+++ Python-daily-work/exampl15.py
@@ -0,0 +1,15 @@
+#W.A.P  to swap/interchange two float values input into stdin
+
+num1=(input("Enter the value"))
+num2=(input("Enter the value"))
+print(F'before swaping num1={num1} and num2={num2}')
+
+num3=num1
+num1=num2
+num2=num3
+print(f'after swaping num1={num1} and num2={num2}')
+#num1,num2=num2,num1
+
+
+
+      
