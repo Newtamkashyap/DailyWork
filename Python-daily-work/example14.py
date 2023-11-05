4945e9798e92757bd004c6e25e3707fd389b9ebd
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-06 00:17:10 +0530

    Add files via upload

---
+++ Python-daily-work/example14.py
@@ -0,0 +1,29 @@
+#changing different different data type into float
+
+a1=float(1.0)
+print(a1)
+
+a2=float(10)
+print(a2)
+
+a3=float("10")
+print(a3)
+
+a4=float(True)
+print(a4)
+
+a5=float(False)
+print(a5)
+
+a6=float("20e-1")
+print(a6)
+
+
+
+#a7=float("abc")
+#print(a7)
+#Traceback (most recent call last):
+ # File "C:/Users/newtam/OneDrive/Desktop/Python_Folder/example14.py", line 21, in <module>
+ #   a7=float("abc")
+#ValueError: could not convert string to float: 'abc'
+
