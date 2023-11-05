2cbcb30fa61953ee9ce46e424aa454d995199aa7
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-06 04:25:16 +0530

    Add files via upload

---
+++ Python-daily-work/example20.py
@@ -0,0 +1,20 @@
+#here we will see how to find ord value in ASCII  by using chr(any ASCII value) function.
+
+#we can find only one char in one time.
+
+#how to find the value in uppercase 
+
+a=chr(66)
+print(a)
+
+b=chr(77)
+print(b)
+
+#how to find the value in lowercase 
+
+
+a1=chr(122)
+print(a1)
+
+b1=chr(88)
+print(b1)
