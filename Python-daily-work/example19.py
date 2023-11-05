b17f8ec4b81a6c31a927b8f87ef05e8b83199590
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-06 04:17:08 +0530

    Add files via upload

---
+++ Python-daily-work/example19.py
@@ -0,0 +1,22 @@
+#here we will see how to find char value in ASCII  by using ord(any char) function.
+
+#we can find only one char in one time.
+
+#how to find the value in uppercase 
+
+a=ord("A")
+print(a)
+
+b=ord("B")
+print(b)
+
+#how to find the value in lowercase 
+
+
+a=ord("b")
+print(a)
+
+b=ord("a")
+print(b)
+
+
