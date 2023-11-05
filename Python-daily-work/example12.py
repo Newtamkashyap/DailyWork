528fd6ae2edb78f491a574d604514cfcbc23a32a
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-05 17:21:05 +0530

    Add files via upload

---
+++ Python-daily-work/example12.py
@@ -0,0 +1,28 @@
+a=10
+b=100
+print(a+b)
+#OR
+print(f'sum of {a} and {b} is = {a+b}')
+
+#OR
+
+a=10
+b=1.5
+print(f'sum of two number{a} and {b} is = {a+b}')
+
+
+#OR
+
+a=28
+b=32
+c=a+b
+#d=decimal format
+#o=octal format
+#x=hexadecimal format
+#b=binary format
+#here we are changing output in all four format:- (d,o,x,b)
+print(f'{a:d},{a:o},{a:x},{a:b}')
+
+
+
+
