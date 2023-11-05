6efc1774cbee91fbf6143a2f4ec21dc4898a4578
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-06 00:56:44 +0530

    Add files via upload

---
+++ Python-daily-work/exampl16.py
@@ -0,0 +1,26 @@
+#changing different different data type into complex by using complex() function
+
+comp1=complex(1+2j)
+print(comp1)
+print(type(comp1))
+
+
+comp2=complex(10)
+print(comp2)
+print(type(comp2))
+
+
+comp3=complex("1000")
+print(comp3)
+print(type(comp3))
+
+
+comp4=complex(True)
+print(comp4)
+print(type(comp4))
+
+comp5=complex(1.2)
+print(comp5)
+print(type(comp5))
+
+
