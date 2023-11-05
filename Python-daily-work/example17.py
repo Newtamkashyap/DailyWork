3d536bda1c08de6259a036ad35aeb0d18fbf9323
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-06 02:50:51 +0530

    Add files via upload

---
+++ Python-daily-work/example17.py
@@ -0,0 +1,11 @@
+#WAP to adding two complex number
+comp1=complex(input("Enter ist value"))
+comp2=complex(input("Enter 2nd value"))
+comp3=comp1+comp2
+print(comp3)
+print(f"sum of two {comp1} and {comp2} is equals to {comp1+comp2}")
+
+#or 
+
+print(f"sum of two {comp1} and {comp2} is equals to {comp3}")
+
