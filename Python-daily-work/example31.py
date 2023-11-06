e2fde413578032e66980ece21af494c068e54bb3
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-07 04:16:16 +0530

    Add files via upload

---
+++ Python-daily-work/example31.py
@@ -0,0 +1,23 @@
+#/ division operator or float division operator.
+#alwz give result as a float type.
+
+x=5/2
+print(x)
+print(type(x))
+
+y=40/10
+print(y)
+
+
+z=4/2.0
+print(z)
+
+a=0/10
+print(a)
+
+b=12/0
+print(b)
+#Traceback (most recent call last):
+#File "C:/Users/newtam/OneDrive/Desktop/Python_Folder/example31.py", line 18, in <module>
+#b=12/0
+#ZeroDivisionError: division by zero
