b54d6edbed97583f90b5145c7f0aa4dcce238c6f
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-07 02:46:17 +0530

    Add files via upload

---
+++ Python-daily-work/example25.py
@@ -0,0 +1,6 @@
+#WAP to add two number.
+#changing int into evalution number by using eval()
+eval1=eval(input("enter ist value"))
+eval2=eval(input("enter 2nd value"))
+eval3=eval1+eval2
+print(f'sum of {eval1} and {eval2} is {eval3}')
