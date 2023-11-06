a3b9bfa28823f88b5959e3f5b673224295ee687f
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-07 02:34:57 +0530

    Add files via upload

---
+++ Python-daily-work/example23.py
@@ -0,0 +1,18 @@
+#Changing different different data type into evalution()
+
+a=eval("1.2")
+print(a)
+print(type(a))
+
+b=eval("25")
+print(b)
+print(type(b))
+
+
+c=eval("(1+2j)")
+print(c)
+print(type(c))
+
+d=("10+20+30")
+print(d)
+print(type(d))
