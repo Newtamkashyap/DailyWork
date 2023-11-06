b4afb02f065924fb5bb745ccd8d4b7e99fd0d808
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-06 05:45:57 +0530

    Add files via upload

---
+++ Python-daily-work/example21.py
@@ -0,0 +1,17 @@
+#converting different different data type into str()function.
+
+s1=str("hellow world")
+print(s1)
+
+s2=str(23)
+print(s2)
+
+s3=str(3.4)
+print(type(s3))
+
+s4=str(1+2j)
+print(s4)
+
+s5=str(True)
+print(s5)
+print(type(s5))
