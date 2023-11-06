70f369b23501c9c4cda4deac357919647ca2d5c6
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-07 02:57:44 +0530

    Add files via upload

---
+++ Python-daily-work/example26.py
@@ -0,0 +1,9 @@
+#WAP to add two number.
+#changing complex number into evalution number by using eval()
+
+eval1=eval(input("enter first complex number"))
+eval2=eval(input("enter first complex number"))
+eval3=eval1+eval2
+print(eval3)
+print(f'sum of {eval1} and {eval2} is {eval3}')
+print(type(eval1),type(eval2),type(eval3))
