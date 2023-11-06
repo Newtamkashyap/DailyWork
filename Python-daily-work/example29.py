e7ce2112120a7b74d4564e1948527659faea58a0
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-07 03:44:25 +0530

    Add files via upload

---
+++ Python-daily-work/example29.py
@@ -0,0 +1,18 @@
+#using list
+#list must be sequence
+#value must be integer
+#here we are listing  for repeating/redundency/duplicating the same values.
+list1=[0]
+list1=list1*10
+print(list1)
+
+
+s1="ABc"
+s1=s1*10
+print(s1)
+
+r1=("_-"*30)
+print(r1)
+
+a=("1"*10)
+print(a)
