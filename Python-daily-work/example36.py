c47fc75e6f2799fc527c8624493a5af18394c1dc
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-07 05:58:31 +0530

    Add files via upload

---
+++ Python-daily-work/example36.py
@@ -0,0 +1,19 @@
+#--- % Modula operator---
+
+rem1=50%3
+print(rem1)
+print(type(rem1))
+
+rem2=40%2
+print(rem2)
+
+
+rem3=3**5
+print(rem3)
+
+
+rest3=10
+print(rest3)
+
+rest4=10**-1
+print(rest4)
