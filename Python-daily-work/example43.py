a10eda3a38ec998c282e18044cc7a3eb99d886e9
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-16 04:28:38 +0530

    Add files via upload

---
+++ Python-daily-work/example43.py
@@ -0,0 +1,7 @@
+#W.A.P to find that person is eligible for vote or not.
+name=(input("enter the name"))
+age=int(input("enter the age"))
+if age>=18:
+    print(f"person is eligible for vote:")
+else:
+    print(f"not eligible")
