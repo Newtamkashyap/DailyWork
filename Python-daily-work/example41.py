9dc185a1bc8aad5dac4cb5bbd13f380749b69b4e
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-16 03:59:38 +0530

    To find square root

---
+++ Python-daily-work/example41.py
@@ -0,0 +1,22 @@
+#square root
+res1=5**2
+print(res1)
+print(type(res1))
+
+
+res2=5**0
+print(res2)
+print(type(res2))
+
+res3=5**-1
+print(res3)
+
+res4=4.5**3
+print(res4)
+print(type(res4))
+
+
+
+res5=1+2j**3
+print(res5,end=' ')
+print(type(res5))
