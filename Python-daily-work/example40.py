2ce744316a944db019ca30543bec8742d28c816e
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-16 03:58:36 +0530

    Add files via upload

---
+++ Python-daily-work/example40.py
@@ -0,0 +1,14 @@
+#6. Write a program to find input number is +ve or –ve
+num=int(input("Enter any number"))
+print("positive number")if num>=0 else print("negative number")
+
+Enter any number0
+positive number
+
+==================================== RESTART: C:/Users/newtam/OneDrive/Desktop/Python_Folder/example37.py ===================================
+Enter any number3
+positive number
+
+==================================== RESTART: C:/Users/newtam/OneDrive/Desktop/Python_Folder/example37.py ===================================
+Enter any number-9
+negative number
