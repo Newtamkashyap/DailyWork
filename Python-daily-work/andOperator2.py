c1fe8536ad891a95ccf292069a8aea0434cc40be
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-17 01:54:31 +0530

    And operator example

---
+++ Python-daily-work/andOperator2.py
@@ -0,0 +1,15 @@
+#login application
+user=input("enter user name:")# lets assume number is newtam
+password=input("enter password")#  lets assume password is new@123
+print("WELCOME") if user== "newtam" and password=="new@123" else print("invalid user  password")
+
+
+
+print("========================================================================")
+phoneNumber=int(input("enter phone number:"))#1234567890
+password=input("enter password:")#123
+if phoneNumber==1234567890 and  password=="#123":
+    print("welcome")
+else:
+    print("invalid")
+
