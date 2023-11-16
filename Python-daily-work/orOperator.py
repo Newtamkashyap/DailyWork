de24c5dfc1c77566ba580b64b4b1340dcf8eb28d
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-17 02:26:39 +0530

    Or Operator example

---
+++ Python-daily-work/orOperator.py
@@ -0,0 +1,12 @@
+#WAP to find input character is vowel or not.
+
+char=input("enter any character")#a,i,e,o,u or A,E,I,O,U
+print("vowel")if char=='a' or char=='e' or char=='i' or char=='o' or char=='u' and char=='A' or char=='I' or char=='O' or char=='E' or char=='U' else print("not vowel")
+
+print("===================in one line also we can write to reduce number of lines====================================================")
+
+char=input("any char")
+if  char=='a' or char=='e' or char=='i' or char=='o' or char=='u' and char=='A' or char=='I' or char=='O' or char=='E' or char=='U':
+    print("vowel")
+else:
+    print("not vowel")
