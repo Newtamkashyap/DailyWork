b2a54f922aa410ba75a2515435f89e29b7fcc8c5
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-17 02:46:10 +0530

    If ELSE example

---
+++ Python-daily-work/ifElse.py
@@ -0,0 +1,6 @@
+#WAP to find input number is +ve ,-ve or zero
+num=int(input("enter any digit:"))
+print("positive number")if num>0 else print("negative") if num<0 else print("zero")
+
+
+              
