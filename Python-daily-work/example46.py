59d8f59406584a0b524c2a6bde6e7839bbd40adf
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2023-11-25 02:11:33 +0530

    list

---
+++ Python-daily-work/example46.py
@@ -0,0 +1,20 @@
+#list
+list1=[]
+print(list1)
+print(type(list1))
+
+list2=[]
+print(list2)
+[]
+
+list3=[10]
+print(list3)
+
+list4=[10,20,30,40,50]
+print(list4)
+[10, 20, 30, 40, 50]
+
+list5=[1,"naresh","python",6000.0]
+       
+print(list5)
+       
